7ce8bef673bd9d58b9f861a1cceee34ee7d97b8c
Vansh Kalra <vanshkalra645@gmail.com>
2024-11-14 21:26:46 +0530

    Finished Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
@@ -6,10 +5,10 @@ import ContactList from './components/ContactList';
 function App() {
   return (
     <div className="App">
-      <h2>Eniro SDE Intern Assessment</h2>
-      <h3>Name: Vansh Kalra</h3>
-      <h3>Email: vanshkalra645@gmail.com</h3>
-      <h3>Resume: <span><a target='_blank' href='https://green-hali-57.tiiny.site/'>link</a></span></h3>
+      <h2 style={{ color: '#2c3e50', textAlign: 'center' }}>Eniro SDE Intern Assessment</h2>
+      <h3 style={{ color: '#34495e', margin: '10px 0' }}>Name: Vansh Kalra</h3>
+      <h3 style={{ color: '#34495e', margin: '10px 0' }}>Email: <span style={{ fontWeight: 'normal' }}>vanshkalra645@gmail.com</span></h3>
+      <h3 style={{ color: '#34495e', margin: '10px 0' }}>Resume: <span><a target='_blank' href='https://green-hali-57.tiiny.site/' style={{ color: '#2980b9', textDecoration: 'underline' }}>link</a></span></h3>
       <ContactForm />
       <ContactList />
     </div>
