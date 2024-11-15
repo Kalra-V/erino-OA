f0eee23bf338c6108b00f542e94fcf0d59677f25
Vansh Kalra <vanshkalra645@gmail.com>
2024-11-16 02:04:33 +0530

    Misspelled "Erino" on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import ContactList from './components/ContactList';
 function App() {
   return (
     <div className="App">
-      <h2 style={{ color: '#2c3e50', textAlign: 'center' }}>Eniro SDE Intern Assessment</h2>
+      <h2 style={{ color: '#2c3e50', textAlign: 'center' }}>Erino SDE Intern Assessment</h2>
       <h3 style={{ color: '#34495e', margin: '10px 0' }}>Name: Vansh Kalra</h3>
       <h3 style={{ color: '#34495e', margin: '10px 0' }}>Email: <span style={{ fontWeight: 'normal' }}>vanshkalra645@gmail.com</span></h3>
       <h3 style={{ color: '#34495e', margin: '10px 0' }}>Resume: <span><a target='_blank' href='https://green-hali-57.tiiny.site/' style={{ color: '#2980b9', textDecoration: 'underline' }}>link</a></span></h3>
